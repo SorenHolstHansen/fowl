3e64df4cef09136d968859ca85024dc4eb017513
Søren Holst Hansen <shol@basecph.com>
2025-11-21 16:36:20 +0100

    Check for the right version of re2rust in lexer build script, and move std to a fowl project with fowl.jsonc

--- libs/lexer/build.rs
+++ libs/lexer/build.rs
@@ -1,6 +1,29 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo::rerun-if-changed=src/lexing.re");
+
+    let mut version_cmd = std::process::Command::new("re2rust");
+    version_cmd.arg("--version");
+    match version_cmd.output() {
+        Ok(v) => {
+            let version_res =
+                String::from_utf8(v.stdout).expect("Expected re2rust --version to print a version");
+            let version = version_res
+                .trim()
+                .split(' ')
+                .last()
+                .expect("Expected re2rust --version to print a version");
+            if !version.starts_with("4") {
+                println!(
+                    "cargo::error=re2rust version 4 required, found '{}'",
+                    version
+                );
+                panic!()
+            }
+        }
+        Err(e) => panic!("{e:?}"),
+    }
+
     let mut cmd = std::process::Command::new("re2rust");
     cmd.args([
         "src/lexing.re",
